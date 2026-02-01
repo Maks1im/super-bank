5d6cb548cfb4aff72c1156ba5dffdc3b7872290d
Maxim <max.lebedev1104@gmail.com>
2026-02-01 14:26:56 +0400

    created header

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,6 +1,18 @@
+import styles from "./Header.module.scss"
+
 const Header = () => {
     return (
-       <header></header>
+       <header className={styles.header}>
+           <h2 className={styles.logo}>super-bank</h2>
+           <nav className={styles.navbar}>
+               <ul className={styles.navbarUl}>
+                   <li><a href="">Main</a></li>
+                   <li><a href="">About us</a></li>
+                   <li><a href="">Contacts</a></li>
+               </ul>
+           </nav>
+           <button className={styles.logInButton}><a href="">Log in</a></button>
+       </header>
     );
 };
 
